88de43f7d7c0bf9197a389bb545f4f0a356fa93b
sarahduv <sarah.duvdevani@gmail.com>
2020-01-30 16:54:25 -0800

    fixing timer

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -545,6 +545,7 @@
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
             this.timer1.Interval = 20;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
