fad69c235c35be85439ff94bcc271aadf7fec093
sarahduv <sarah.duvdevani@gmail.com>
2020-01-30 16:59:54 -0800

    changing label 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -537,6 +537,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(477, 26);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(92, 31);
